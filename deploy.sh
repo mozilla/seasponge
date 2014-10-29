dbc4186e51451e95a682499ebf2b4adbdd3fa8de
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-10-29 01:08:23 -0300

    fix(deploy): workaround for using dist/bower_components/ for fonts
    See bug screenshot at
    https://github.com/mozilla/seasponge/commit/de7f5a66b662936abc89070d5aa5776562605641#commitcomment-8344709
    
    See #32.

--- deploy.sh
+++ deploy.sh
@@ -31,14 +31,17 @@ if [[ -z $(git status -s) ]]; then
     echo "Going to building the app into directory ${destDir}/"
     grunt build
 
+    # Temporarily move bower_components/ directory
+    mkdir .tmp/ || true
+    mv bower_components/ .tmp/bower_components/ || true
+    
     # Checkout Deploy branch
-    echo "Checking out branch, ${deployBranch}"
     git checkout "$deployBranch"
 
     # Clean deploy directory
     echo "Cleaning up deploy branch by removing old build files"
-    find . -maxdepth 1 | grep -v "\./README\.md\|\./LICENSE\|\.git\|\./${destDir}\|\./node_modules\|\./bower_components\|\.$" | xargs rm -r
-
+    find . -maxdepth 1 | grep -v "\./README\.md\|\./LICENSE\|\.git\|\./${destDir}\|\./node_modules\|\./bower_components\|\.tmp\|\.$" | xargs rm -r
+    
     # Move up dist/ to current directory for deploying
     echo "Move the build files in ${destDir}/ up to the current directory for deploying"
     mv ${destDir}/* .[^.]* . || true
@@ -57,6 +60,10 @@ if [[ -z $(git status -s) ]]; then
     
     echo "Checking out original branch, ${currentBranch}"
     git checkout "${currentBranch}"
+    
+    # Move original bower_components/ back now that we are back in stable branch
+    rm -r bower_components/ || true # Remove the dist/bower_components/
+    mv .tmp/bower_components/ ./bower_components || true # Restore developer's bower_components/
     
     echo "Done!"
     echo "";
